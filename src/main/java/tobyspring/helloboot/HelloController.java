b36c44bb559c76854086e2d4926743e6ffe4c7db
inseooo0 <calmandpassion@korea.ac.kr>
2025-03-21 14:00:51 +0900

    test: add unit test

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,25 +1,22 @@
 package tobyspring.helloboot;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-
 @RestController
 public class HelloController {
     private final HelloService helloService;
-    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+    public HelloController(HelloService helloService) {
         this.helloService = helloService;
-        this.applicationContext = applicationContext;
-        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
     public String hello(@RequestParam("name") String name) {
-        return helloService.sayHello(Objects.requireNonNull(name));
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return helloService.sayHello(name);
     }
 }
