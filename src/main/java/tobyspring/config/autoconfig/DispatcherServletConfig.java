bd1f2a285bcdc9d1e8e5a89edb146fc0de68402e
inseooo0 <calmandpassion@korea.ac.kr>
2025-03-21 17:27:56 +0900

    test: add proxyBeanMethods study test code

--- src/main/java/tobyspring/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/tobyspring/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package tobyspring.config.autoconfig;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.config.MyAutoConfiguration;
 
-@Configuration
+@MyAutoConfiguration // MyAutoConfiguration에 의해 설정되기 때문에 붙임(관례)
 public class DispatcherServletConfig {
 
     @Bean
