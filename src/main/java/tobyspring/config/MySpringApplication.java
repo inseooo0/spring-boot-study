cc353c55c69aa1ca8c3fd9aaca6062ed533dbbbf
inseooo0 <calmandpassion@korea.ac.kr>
2025-03-21 15:49:41 +0900

    feat: add auto configuration

--- src/main/java/tobyspring/config/MySpringApplication.java
+++ src/main/java/tobyspring/config/MySpringApplication.java
@@ -1,4 +1,4 @@
-package tobyspring.helloboot;
+package tobyspring.config;
 
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
