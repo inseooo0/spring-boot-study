c0b3bcc4022ab29c1c4b05137a5c03c9f89f1ff0
inseooo0 <calmandpassion@korea.ac.kr>
2025-03-21 14:56:18 +0900

    fix: add composite annotation

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,28 +1,10 @@
 package tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan
+@MySpringBootAnnotation
 public class HellobootApplication {
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet(); // spring container가 bean 생성 시에 applicationContext를 대신 주입해줌
-    }
-
     public static void main(String[] args) {
-        // MySpringApplication.run(HellobootApplication.class, args);
         SpringApplication.run(HellobootApplication.class, args);
     }
 }
